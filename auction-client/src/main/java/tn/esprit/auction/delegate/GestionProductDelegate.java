682b531030be327c8507ac05fb0aec5bb303fc42
sabrine.khoualdi <sabrine.khoualdi@esprit.tn>
2015-02-15 17:43:17 +0100

    TestGestionProduct+GestionProductDelegate

---
+++ auction-client/src/main/java/tn/esprit/auction/delegate/GestionProductDelegate.java
@@ -0,0 +1,20 @@
+package tn.esprit.auction.delegate;
+
+import tn.esprit.auction.domain.Product;
+import tn.esprit.auction.locator.ServiceLocator;
+import tn.esprit.auction.services.gestion.product.ProductServicesRemote;
+
+public class GestionProductDelegate {
+	static ProductServicesRemote remote;
+	private static final String jndi="auction-ejb/ProductServices!tn.esprit.auction.services.gestion.product.ProductServicesRemote"; 
+
+	private static ProductServicesRemote getProxy(){
+		return (ProductServicesRemote) ServiceLocator.getInstance().getProxy(jndi);
+	}
+	
+	public  static Boolean doAddProduct(Product product){
+	return getProxy().addProduct(product);
+	
+}
+	
+}
